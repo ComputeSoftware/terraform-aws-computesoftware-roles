d6f5c1af0498a121c3e52be1a90dc17839939a13
Kenny Williams <kennyjwilli@gmail.com>
2020-09-28 09:20:00 -0700

    Add platform_role_external_id output

--- examples/simple-roles/outputs.tf
+++ examples/simple-roles/outputs.tf
@@ -2,6 +2,10 @@ output "platform_role_arn" {
   value = module.computesoftware-roles.platform_role_arn
 }
 
+output "platform_role_external_id" {
+  value = module.computesoftware-roles.platform_role_external_id
+}
+
 output "discovery_role_arn" {
   value = module.computesoftware-roles.discovery_role_arn
 }
--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "platform_role_arn" {
   description = "The IAM Role ARN for the Compute Software Platform Role."
 }
 
+output "platform_role_external_id" {
+  value = var.platform_role_external_id
+  description = "The External ID provided to you from Compute Software."
+}
+
 output "discovery_role_arn" {
   value = aws_iam_role.discovery_role.arn
   description = "The IAM Role ARN for the Discovery Role."
