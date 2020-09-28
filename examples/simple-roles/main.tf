f8ed0b648ae2528704ef62838ff4ef16c85184d0
Kenny Williams <kenny@computesoftware.com>
2020-09-27 18:33:21 -0700

    Remote unneeded vars

--- examples/simple-roles/main.tf
+++ examples/simple-roles/main.tf
@@ -5,6 +5,4 @@ provider "aws" {
 module "roles" {
   source = "../.."
   platform_role_external_id = "abc123"
-  discovery_role_name = "cs-discovery-test"
-  platform_role_name = "cs-platform-test"
 }
