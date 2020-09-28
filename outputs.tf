5558d593f20842a2b5aefef8db4978d8acd1c313
Kenny Williams <kennyjwilli@gmail.com>
2020-09-28 08:16:18 -0700

    Add output descriptions.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "platform_role_arn" {
   value = aws_iam_role.platform_role.arn
+  description = "The IAM Role ARN for the Compute Software Platform Role."
 }
 
 output "discovery_role_arn" {
   value = aws_iam_role.discovery_role.arn
+  description = "The IAM Role ARN for the Discovery Role."
 }
